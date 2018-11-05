4a838bf9c88bc82b04cb574aa9f81e4eaf1c0fbb
serge <serge.goryachev@gmail.com>
2018-11-06 01:33:04 +0300

    создали таблицу

---
+++ database/migrations/2018_11_05_222533_create_price_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePriceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('price', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('articul')->nullable();
+            foreach (range(0, 5) as $item){
+                $table->integer('price_'.$item)->nullable();
+            }
+            $table->integer('qty')->nullable();
+            $table->json('data')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('price');
+    }
+}
